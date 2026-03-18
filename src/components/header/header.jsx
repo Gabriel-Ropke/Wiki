9e8f34bd20fb8132b4f78f0d5e5989a7f04f1f69
Gabriel-Ropke <ropkegabriel@gmail.com>
2026-03-18 14:37:26 -0300

    config deploy

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,25 +3,27 @@ import logo from "../../assets/Logo.png";
 
 export function Header({ isOpen, toggleMenu }) {
   return (
-    <header className={isOpen ? "" : "open"}>
+    <>
       <button className="menu-btn" onClick={toggleMenu}>
         <span className={isOpen ? "open" : ""}></span>
       </button>
+      <header className={isOpen ? "" : "open"}>
+        <div></div>
+        <div className="logo-container">
+          <a href="#home">
+            <img src={logo} alt="" />
+          </a>
+        </div>
 
-      <div className="logo-container">
-        <a href="#home">
-          <img src={logo} alt="" />
-        </a>
-      </div>
-
-      <ul className="login-header-area">
-        <li>
-          <a href="#login">Log In</a>
-        </li>
-        <li>
-          <a href="#register">Register</a>
-        </li>
-      </ul>
-    </header>
+        <ul className="login-header-area">
+          <li>
+            <a href="#login">Log In</a>
+          </li>
+          <li>
+            <a href="#register">Register</a>
+          </li>
+        </ul>
+      </header>
+    </>
   );
 }
